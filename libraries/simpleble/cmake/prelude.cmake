adcc955260e9978de51d51b64aafcb1df144884d
dennisppaul <dennis.paul@hfk-bremen.de>
2023-08-02 08:34:16 +0200

    added SimpleBLE as build-able library

---
+++ libraries/simpleble/cmake/prelude.cmake
@@ -0,0 +1,10 @@
+cmake_minimum_required(VERSION 3.21)
+
+set(CMAKE_OSX_ARCHITECTURES "arm64;x86_64")
+
+# Common configuration used by all projects in this repository
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/find)
+
+include(${CMAKE_CURRENT_LIST_DIR}/parse_version.cmake)
+parse_version(${CMAKE_CURRENT_LIST_DIR}/../VERSION SIMPLEBLE)
